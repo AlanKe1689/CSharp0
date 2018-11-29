3722235c318ad4d9e445959d62d85aba4f506925
AlanKe1689 <44349571+AlanKe1689@users.noreply.github.com>
2018-11-28 23:17:22 -0800

    Add files via upload

---
+++ COMP2614Assign05/COMP2614Assign05/DateValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2614Assign05
+{
+    class DateValidator
+    {
+        private const int MIN_YEAR = 1900;
+
+        public static bool Validate(string year, string month, string day)
+        {
+            bool isValid = false;
+
+            try
+            {
+                DateTime date = new DateTime(int.Parse(year), int.Parse(month), int.Parse(day));
+
+                if (date.Year >= MIN_YEAR)
+                {
+                    isValid = true;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return isValid;
+        }
+    }
+}
---
+++ COMP2614Assign05/COMP2614Assign05/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP2614Assign05
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            labelResult.Text = string.Empty;
+            textBoxYear.Focus();
+        }
+
+        private void textBoxYear_Enter(object sender, EventArgs e)
+        {
+            textBoxYear.SelectAll();
+        }
+
+        private void textBoxMonth_Enter(object sender, EventArgs e)
+        {
+            textBoxMonth.SelectAll();
+        }
+
+        private void textBoxDay_Enter(object sender, EventArgs e)
+        {
+            textBoxDay.SelectAll();
+        }
+
+        private void checkDateButton_Click(object sender, EventArgs e)
+        {
+            bool isValid = DateValidator.Validate(textBoxYear.Text, textBoxMonth.Text, textBoxDay.Text);
+
+            if (isValid)
+            {
+                labelResult.ForeColor = Color.Green;
+                labelResult.Text = "Valid";
+                toolTipResult.SetToolTip(labelResult, "Entered Date is Valid");
+            }
+            else
+            {
+                labelResult.ForeColor = Color.Red;
+                labelResult.Text = "Invalid";
+                toolTipResult.SetToolTip(labelResult, "Entered Date is NOT Valid");
+            }
+        }
+    }
+}
